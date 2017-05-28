7da9ca90d8c30c3aea93a906f37e194bf36326d0
Husna Sayedi <hsayedi95@gmail.com>
2017-05-28 12:40:29 -0700

    Discrete Fourier Transformation
    The Discrete Fourier Transform (DFT) of a periodic array fj , for j = 0; 1;;;N (correspond-
    ing to data at equally spaced points, starting at the left end point of the interval of periodicity) is
    evaluated via the Fast Fourier Transform (FFT) algorithm (N power of 2). Using a FFT package,
    i.e. an already coded FFT

---
+++ 104AHW5num3.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 15 16:59:41 2016
+
+@author: Husna
+"""
+
+# Homework 5 Number 2 
+import math
+import numpy 
+
+
+def ck(f,N):
+    c = np.fft.fft(f)
+    ak = c.real*(m)
+    bk = c.imag*(m)
+    return(ak,bk)
+
+def P(x,N,a,b):
+    for j in range(1,m):
+        sum=a[j]*math.cos(j*x)+b[j]*math.sin(j*x)+sum
+    P = .5*a[0]+sum+.5*a[N/2]*math.cos(m*x)
+    return(P)
+
+def Prime(a,b,xj,m):
+    for j in range(1,m):
+        sum = (-j)*a[j]*math.sin(j*xj)+j*b[j]*math.cos(j+xj)
+
+for j in range(0,N):
+    x[j]= j*(2*math.pi)/N    
+    f[j]= math.exp(math.sin(x[j]))
+    
+N = 8
+m =int(round(N/2,2.0))
+f = [0]*N
+x = [0]*N
+prime=[0]*N
+for j in range(0,N):
+    x[j]=j*2*math.pi/N
+    f[j]=math.exp(math.sin(x[j])
+    prime=Prime(a,b,x[j],m)
+   
+
+
+#Y = (-5.79829066331+4.55640490659j)
+#
+#Z = (Y.real, Y.imag)
+#
+#A = Y.real
+#B = Y.imag
+#print(Z)
+#    
+
+
+
+
+# Problem 4 
+
+N = 8
+m =int(round(N/2,2.0))
+f = [0]*N
+x = [0]*N
+prime=[0]*N
+for j in range(0,N):
+    x[j]=j*2*math.pi/N
+    f[j]=math.exp(math.sin(x[j])
+   
+    
+c=np.fft.fft(f)
+print(c)
+
+
+
+
+
+
+
+
+
+
+
