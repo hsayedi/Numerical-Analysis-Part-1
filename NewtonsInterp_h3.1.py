a711b5488b287ae5eb408fe8145c03408331999d
Husna Sayedi <hsayedi95@gmail.com>
2017-05-27 23:18:35 -0700

    Update NewtonsInterp_h3.1.py

--- NewtonsInterp_h3.1.py
+++ NewtonsInterp_h3.1.py
@@ -32,3 +32,47 @@ def newton(x,f,n,a):
 
 new=newton(x,f,n,a)
 print(new)
+
+
+# Homework 3 num 3 part B
+# computing coefficients to evaluate the corresponding interpolation polynomial at arbitary point x
+
+
+import math
+import matplotlib.pyplot as plt
+    
+def newton(x,f,n,a):
+    c = [0]*(n+1)
+    for j in range(0,n+1):
+        c[j]=f[j]
+    for k in range(1,n+1):
+        for j in range(n,k-1,-1):
+            c[j] = (c[j]-c[j-1])/(x[j]-x[j-k])
+
+    p=c[n]
+    for j in range(n-1,-1,-1):
+        p=c[j]+(a-x[j])*p          # <---p(a)
+    return p
+
+
+def f(x):
+    return math.exp(-x**2)
+n = 10   
+x = [0]*11
+g = [0]*11
+for j in range(0,11):
+        x[j] = -1+j*(2/10)
+        g[j] = f(x[j])
+xbar=[0]*101
+p=[0]*101
+fbar=[0]*101
+dif=[0]*101
+for j in range(0,101):
+        xbar[j] = -1+j*(2/100)
+        p[j] = newton(x,g,n,xbar[j])
+        fbar[j] = f(xbar[j])
+        dif[j]=fbar[j]-p[j]
+plt.plot(xbar,dif)    
+
+
+
